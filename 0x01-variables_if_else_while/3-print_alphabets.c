c05fbe9a89e428180b9a16168671c1dc6f34115f
Abdullah Mohamed <abdomid90@gmail.com>
2023-03-20 17:59:10 +0200

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char c;
+        char c1;
+
+        c = 'a';
+        c1 = 'A';
+        while
+        (c <= 'z') {
+        putchar(c);
+        c++;
+        }
+        while 
+        (c1 <= 'Z') {
+        putchar(c1);
+        c1++;
+        }
+        return (0);
+}
