a0d7acec6b6e54e856e2dc6726e44c9ac5437223
Abdullah Mohamed <abdomid90@gmail.com>
2023-03-22 00:10:49 +0200

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the in lowercase
+ * Return: always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+	int a;
+
+	for (a = 1; <= 10; a++)
+	{
+		for (i = 97; i = 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
