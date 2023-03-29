e3ac52b81d87d52b49de4f4a01e52ac1ca974f04
Abdullah Mohamed <abdomid90@gmail.com>
2023-03-29 23:32:35 +0200

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,9 @@ char *_strcat(char *dest, char *src)
 
 {
 	int i;
+
 	int j;
+
 	i = 0;
 
 	while (dest[i] != '\0')
@@ -20,6 +22,7 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 	j = 0;
+
 	while (src[j] != '\0')
 
 	{
